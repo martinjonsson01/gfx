545ef7a8c5fd06d17e6f3d166315171f9825fa4f
Martin <martinjonsson01@gmail.com>
2022-12-18 12:03:08 +0100

    Create Rust workspace and projects

---
+++ recs-gfx/benches/gfx_bench.rs
@@ -0,0 +1,21 @@
+﻿use criterion::{criterion_group, criterion_main, Bencher, BenchmarkId, Criterion};
+use std::time::Duration;
+
+fn criterion_benchmark(c: &mut Criterion) {
+    let mut group = c.benchmark_group(group_name);
+    group.measurement_time(Duration::from_secs(10));
+    group.sample_size(1000);
+    group.bench_with_input(
+        BenchmarkId::from_parameter(format!("10^{magnitude:?}")),
+        &search_up_to,
+        || println!("No benchmark implemented yet"),
+    );
+    group.finish();
+}
+
+criterion_group! {
+    name = benches;
+    config = Criterion::default();
+    targets = criterion_benchmark
+}
+criterion_main!(benches);
